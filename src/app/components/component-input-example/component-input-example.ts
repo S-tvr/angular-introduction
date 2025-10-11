9cbd1a5ec5e4e3a72dd228c0f59b86ff3383d896
S_tvr <stav.efth@gmail.com>
2025-10-11 19:12:14 +0000

    feat: welcome and person input example (using person-table-component)

--- src/app/components/component-input-example/component-input-example.ts
+++ src/app/components/component-input-example/component-input-example.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
+import { PersonTable } from '../person-table/person-table';
 
 @Component({
   selector: 'app-component-input-example',
-  imports: [],
+  imports: [PersonTable],
   templateUrl: './component-input-example.html',
   styleUrl: './component-input-example.css'
 })
 export class ComponentInputExample {
+  person0: Person = {
+    givenName: "Christodoulos",
+    surName: "Fragoudakis",
+    age: 23,
+    email: "chris@aueb.gr",
+    address: "Athens, Greece"
+  }
 
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 45,
+    email: 'John@aueb.gr',
+    address: 'New York, USA'
+  }
 }
