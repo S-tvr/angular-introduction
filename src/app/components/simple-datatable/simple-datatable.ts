44f9a6e5f27a56850561bfa49eaa824c5ad1be3c
S_tvr <stav.efth@gmail.com>
2025-10-12 21:10:50 +0000

    feat: sort the data-table using lodash-es sortBy.

--- src/app/components/simple-datatable/simple-datatable.ts
+++ src/app/components/simple-datatable/simple-datatable.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EPerson } from 'src/app/shared/interfaces/eperson';    // so that I can use interface for check
+import { sortBy } from 'lodash-es'
 
 @Component({
   selector: 'app-simple-datatable',
@@ -9,6 +10,33 @@ import { EPerson } from 'src/app/shared/interfaces/eperson';    // so that I can
 })
 export class SimpleDatatable {
   @Input() data: EPerson[] | undefined
+
+  sortOrder = {
+    givenName: "none",
+    surName: "none",
+    age: "none",
+    email: "none",
+    education: "none"
+  }
+
+  sortData(sortKey: keyof EPerson) : void {
+    console.log(sortKey)
+    if (this.sortOrder[sortKey] === "asc") {
+      this.sortOrder[sortKey] = "desc"
+      this.data = sortBy(this.data, sortKey).reverse()
+    } else {
+      this.sortOrder[sortKey] = "asc"
+      this.data = sortBy(this.data, sortKey)
+    }
+
+    for (let key in this.sortOrder) {
+      if(key !== sortKey) {
+        this.sortOrder[key as keyof EPerson] = "none"
+      }
+    }
+    console.log(this.sortOrder)
+
+  }
 
   onPersonClicked(person: EPerson) {
     console.log("Person clicked: ", person)
