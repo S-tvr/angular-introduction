3ff82766354bbb90537d24e8215c5b6530176ec5
S_tvr <stav.efth@gmail.com>
2025-10-15 22:48:59 +0000

    feat: template form read from child and some extras to moving from template to component

--- src/app/components/eperson-template-driven-form/eperson-template-driven-form.ts
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -21,7 +21,23 @@ import { EPerson } from 'src/app/shared/interfaces/eperson';
   styleUrl: './eperson-template-driven-form.css'
 })
 export class EpersonTemplateDrivenForm {
+  @ViewChild('eForm', {static: false}) form:NgForm | undefined
+
   onSubmit(value: EPerson) {
     console.log(value)
+    console.log(this.form)
+    console.log(this.form?.form.get("givenName")?.value)
+    console.log(this.form?.form.controls["surName"].value)
+  }
+
+  onSetValue() {
+    this.form?.setValue({
+      givenName: "John",
+      surName: "Doe",
+      age: 30,
+      email: "aaa@aueb.com",
+      education: "Bachelor's Degree"
+    })
+    this.form?.form.controls['givenName'].setValue("aaaaa")
   }
 }
