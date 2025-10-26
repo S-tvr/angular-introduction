3bab4f4dc17e4b12c05d76bfae0ac849f588d199
S_tvr <stav.efth@gmail.com>
2025-10-26 14:35:41 +0000

    feat: change the navbar and make an material icon to appear based on either user$ is null or has data (i.e user is logged in or not)

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
+import { UserService } from 'src/app/shared/services/user';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  userService = inject(UserService)
+  user = this.userService.user$
+
+  logout() {
+    console.log("Logout")
+  }
 
 }
