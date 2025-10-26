66786b3bead90e79973a8006af7c713623691f86
S_tvr <stav.efth@gmail.com>
2025-10-26 00:07:22 +0000

    feat: make the form and add some functionality. there is an error to be fixed in the confirm password

--- src/app/components/user-registration-for-backend/user-registration-for-backend.ts
+++ src/app/components/user-registration-for-backend/user-registration-for-backend.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -17,11 +17,31 @@ export class UserRegistrationForBackend {
     name: new FormControl('', Validators.required),
     surname: new FormControl('', Validators.required),
     email: new FormControl('',[Validators.required, Validators.email]),
-    address: new FormControl({
+    address: new FormGroup({
       area: new FormControl(''),
       road: new FormControl('')
     }),
     password: new FormControl('', [Validators.required, Validators.minLength(5)]),
     confirmPassword: new FormControl('', [Validators.required, Validators.minLength(5)])
-  })
+  },
+    this.passwordConfirmValidator,
+  )
+
+  passwordConfirmValidator(control: AbstractControl): {[key:string]: boolean} | null {
+    const form = control as FormGroup
+    const password = form.get('password')?.value
+    const confirmPassword = form.get('confirmPassword')?.value
+
+    if(password && confirmPassword && password!== confirmPassword){
+      form.get('confirmPassword')?.setErrors({passwordMissmatch: true})
+      return{passwordMissmatch: true}
+    }
+
+    return null
+  }
+
+  onSubmit() {
+    const data = this.form.value
+    console.log(data)
+  }
 }
