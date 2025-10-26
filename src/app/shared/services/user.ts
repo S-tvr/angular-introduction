e5b88de4d1b5349df67e3754149f973a964044dc
S_tvr <stav.efth@gmail.com>
2025-10-26 22:43:20 +0000

    feat: add wxpiry on auth-guard

--- src/app/shared/services/user.ts
+++ src/app/shared/services/user.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { User, Credentials, LoggedInUser } from '../interfaces/user';
 import { Router } from '@angular/router';
+import { jwtDecode } from 'node_modules/jwt-decode/build/cjs';
 
 const API_URL = `${environment.apiURL}/api/users`
 const API_URL_AUTH = `${environment.apiURL}/api/auth`
@@ -45,5 +46,24 @@ export class UserService {
       this.user$.set(null)
       localStorage.removeItem("access_token")
       this.router.navigate(["login"])
+    }
+
+    isTokenExpired() : boolean {
+      const token = localStorage.getItem('access_token')
+      if (!token) return true
+      
+      try {
+        const decoded = jwtDecode(token)
+        const exp = decoded.exp
+        const now = Math.floor(Date.now()/1000)
+        if (exp) {
+          return exp < now
+        } else {
+          return true
+        }
+
+      } catch (err) {
+        return true
+      }
     }
 }
