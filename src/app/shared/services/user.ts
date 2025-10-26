488cfeb194e0bf3faec7a854b877a48f68b1a0c9
S_tvr <stav.efth@gmail.com>
2025-10-26 15:38:30 +0000

    feat: effect. when sth changes on a signal variable, it doesnt matter where we are, the system knows and can run a function

--- src/app/shared/services/user.ts
+++ src/app/shared/services/user.ts
@@ -1,4 +1,4 @@
-import { Injectable,inject, signal } from '@angular/core';
+import { Injectable,inject, signal, effect } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 import { User, Credentials, LoggedInUser } from '../interfaces/user';
@@ -14,6 +14,16 @@ export class UserService {
   http: HttpClient = inject(HttpClient)
   router = inject(Router)
   user$ = signal<LoggedInUser | null>(null)
+
+  constructor () {
+    effect(() => {
+      if(this.user$()) {
+        console.log("Effect-> User logged in", this.user$()?.username)
+      } else {
+        console.log("Effect-> No user logged in")
+      }
+    })
+  }
 
   registerUser(user : User) {
     return this.http.post<{status:boolean, data:User}>(
